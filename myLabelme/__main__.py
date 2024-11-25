a7621e55062a8935011731e24b884de1d6a48a9d
Esam-HM <isamhalimeh@gmail.com>
2024-11-25 17:37:33 +0300

    Updating main file

--- myLabelme/__main__.py
+++ myLabelme/__main__.py
@@ -4,18 +4,17 @@ import logging
 import os
 import os.path as osp
 import sys
-
 import yaml
+
 from qtpy import QtCore
 from qtpy import QtWidgets
 
-from labelme import __appname__
-from labelme import __version__
-from labelme.app import MainWindow
-from labelme.config import get_config
-from labelme.logger import logger
-from labelme.utils import newIcon
-
+from . import __appname__
+from . import __version__
+from .app import MainWindow
+from .config import get_config
+from .logger import logger
+from .utils import newIcon
 
 def main():
     parser = argparse.ArgumentParser()
